833ad13a297f5540da89ed95fbd25d252f997fbb
Jordan-Methods <jordan.diedong@methods.co.uk>
2024-11-01 15:22:36 +0000

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,22 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource.js';
-import { data } from './data/resource.js';
+import { defineBackend } from "@aws-amplify/backend";
+import { auth } from "./auth/resource";
+import { data, AGENT_ID, generateHaikuFunction } from "./data/resource";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 
-defineBackend({
+export const backend = defineBackend({
   auth,
   data,
+  generateHaikuFunction,
 });
+
+backend.generateHaikuFunction.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    effect: Effect.ALLOW,
+    actions: [
+      "bedrock:InvokeAgent"
+    ],
+    resources: [
+      `arn:aws:bedrock:eu-west-2:412381744271:agent/WLCUYDRNTK` // Specific agent resource
+    ],
+  })
+);
